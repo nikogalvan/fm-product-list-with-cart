9204be14acb53decce94b8bfec1f0b1950cbd5d3
nikogalvan <ramiro_coronel333@hotmail.com>
2025-02-04 14:13:02 -0300

    feat: add icon cart to ProductCart component

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,24 +1,28 @@
 import React from "react";
 import styles from "./ProductCard.module.scss";
 import { Product } from "../../types";
-
+import iconAddToCart from "../../../public/assets/images/icon-add-to-cart.svg";
 interface ProductCardProps {
   product: Product;
 }
 //TODO: Complete this component with funcionality and style
-const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
-  return (
-    <div className={styles.productCard}>
-      <picture>
-        <source media="(min-width:1440px)" srcSet={product.image.desktop} />
-        <source media="(min-width:768px)" srcSet={product.image.tablet} />
+const ProductCard: React.FC<ProductCardProps> = ({ product }) => (
+  <div className={styles.productCard}>
+    <picture>
+      <source media="(min-width:1440px)" srcSet={product.image.desktop} />
+      <source media="(min-width:768px)" srcSet={product.image.tablet} />
+      <div className={styles.imageContainer}>
         <img src={product.image.mobile} alt={product.name} loading="lazy" />
-      </picture>
-      <p className={styles.category}>{product.category}</p>
-      <h3 className={styles.name}>{product.name}</h3>
-      <p className={styles.price}>${product.price.toFixed(2)}</p>
-    </div>
-  );
-};
+        <button className={styles.button}>
+          <img src={iconAddToCart} className={styles.iconCart} />
+          Add to cart
+        </button>
+      </div>
+    </picture>
+    <p className={styles.category}>{product.category}</p>
+    <h3 className={styles.name}>{product.name}</h3>
+    <p className={styles.price}>${product.price.toFixed(2)}</p>
+  </div>
+);
 
 export default ProductCard;
