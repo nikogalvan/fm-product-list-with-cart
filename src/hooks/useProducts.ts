96b46f9269a27947821944b5b3b5470c5f5c714a
nikogalvan <ramiro_coronel333@hotmail.com>
2025-02-07 10:46:15 -0300

    feat: add useProducts custom hook

---
+++ src/hooks/useProducts.ts
@@ -0,0 +1,39 @@
+import { useCallback, useEffect, useState } from "react";
+import { Product, UseProductResult } from "../types";
+
+export const useProducts = (url: string): UseProductResult => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchProducts = useCallback(async () => {
+    try {
+      setLoading(true);
+
+      const resp = await fetch(url);
+      if (!resp.ok) throw new Error(`HTTP error: Status: ${resp.status}`);
+
+      const data: Product[] = await resp.json();
+
+      const productsWithId: Product[] = data.map(
+        (product: Omit<Product, "id">, index: number) => ({
+          ...product,
+          id: crypto.randomUUID(),
+        })
+      );
+
+      setProducts(productsWithId);
+    } catch (err) {
+      setError((err as Error).message);
+      console.error("Error fetching products:", (err as Error).message);
+    } finally {
+      setLoading(false);
+    }
+  }, [url]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+
+  return { products, loading, error };
+};
