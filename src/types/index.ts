1d7bc715d0b5c5d1ee593c9c6db028186f62020e
nikogalvan <ramiro_coronel333@hotmail.com>
2025-02-07 10:42:17 -0300

    feat: add UseProduct interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,18 @@
 export interface Product {
+  id: string;
   image: {
-    mobile: string;
-    desktop: string;
-    thumbnail: string;
-    tablet: string;
+    mobile?: string;
+    desktop?: string;
+    thumbnail?: string;
+    tablet?: string;
   };
   price: number;
   name: string;
   category: string;
+}
+
+export interface UseProductResult {
+  products: Product[];
+  loading: boolean;
+  error: string | null;
 }
