a406dfec73a089eeeb8f5f02bd7ca33ddcef6624
nikogalvan <ramiro_coronel333@hotmail.com>
2025-02-09 13:30:00 -0300

    feat(productcard): integrate ButtonProduct component and modify structure

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,28 +1,26 @@
-import React from "react";
 import styles from "./ProductCard.module.scss";
 import { Product } from "../../types";
-import iconAddToCart from "../../../public/assets/images/icon-add-to-cart.svg";
-interface ProductCardProps {
+import { ButtonProduct } from "../ButtonProduct/ButtonProduct";
+
+interface Props {
   product: Product;
 }
-//TODO: Complete this component with funcionality and style
-const ProductCard: React.FC<ProductCardProps> = ({ product }) => (
-  <div className={styles.productCard}>
-    <picture>
-      <source media="(min-width:1440px)" srcSet={product.image.desktop} />
-      <source media="(min-width:768px)" srcSet={product.image.tablet} />
-      <div className={styles.imageContainer}>
-        <img src={product.image.mobile} alt={product.name} loading="lazy" />
-        <button className={styles.button}>
-          <img src={iconAddToCart} className={styles.iconCart} />
-          Add to cart
-        </button>
-      </div>
-    </picture>
-    <p className={styles.category}>{product.category}</p>
-    <h3 className={styles.name}>{product.name}</h3>
-    <p className={styles.price}>${product.price.toFixed(2)}</p>
-  </div>
-);
-
-export default ProductCard;
+export const ProductCard = ({ product }: Props) => {
+  return (
+    <div className={styles.container}>
+      <picture>
+        <source media="(min-width:1440px)" srcSet={product.image.desktop} />
+        <source media="(min-width:768px)" srcSet={product.image.tablet} />
+        <div className={styles.imageContainer}>
+          <img src={product.image.mobile} alt={product.name} loading="lazy" />
+          <ButtonProduct product={product} />
+        </div>
+      </picture>
+      <footer className={styles.descriptionContainer}>
+        <span className={styles.category}>{product.category}</span>
+        <h3 className={styles.name}>{product.name}</h3>
+        <p className={styles.price}>${product.price.toFixed(2)}</p>
+      </footer>
+    </div>
+  );
+};
