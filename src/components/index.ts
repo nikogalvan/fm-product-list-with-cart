fa23ffeb2f47d5d7f4a33fccfdfebbd0734dbe76
nikogalvan <ramiro_coronel333@hotmail.com>
2025-02-09 13:23:09 -0300

    feat(components): re-export components from a single entry point

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export * from "./CartCard/CartCard.tsx";
+export * from "./ProductCard/ProductCard.tsx";
+export * from "./ButtonProduct/ButtonProduct.tsx";
+export * from "./ProductList/ProductList.tsx";
