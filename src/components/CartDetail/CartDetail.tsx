3b5862ca31b90c8adbf0e70bfdc5b13e10154a3b
nikogalvan <ramiro_coronel333@hotmail.com>
2025-02-12 21:13:58 -0300

    feat(CartDetail): changing structure of component

--- src/components/CartDetail/CartDetail.tsx
+++ src/components/CartDetail/CartDetail.tsx
@@ -1,24 +1,35 @@
 import styles from "./CartDetail.module.scss";
+import removeFromCartSVG from "/assets/images/icon-remove-item.svg";
 interface Props {
   name: string;
   quantity: number;
   price: number;
   handleRemoveItem: () => void;
 }
-
 const CartDetail: React.FC<Props> = ({
   name,
   quantity,
   price,
   handleRemoveItem,
 }) => {
+
+  const totalPrice = (price * quantity).toFixed(2);
+  const formattedPrice = price.toFixed(2);
   return (
-    <div className={styles.CartDetail}>
-      <p>{name}</p>
-      <p>{quantity}x</p>
-      <p>@ ${price.toFixed(2)}</p>
-      <p>${(price * quantity).toFixed(2)}</p>
-      <button onClick={handleRemoveItem}>X</button>
+    <div className={styles.container}>
+      <div className={styles.productInfo}>
+        <p className={styles.productName}>{name}</p>
+        <div className={styles.productDetails}>
+          <p className={styles.quantity}>{quantity}x</p>
+          <div className={styles.priceInfo}>
+            <p className={styles.unitPrice}>@ ${formattedPrice}</p>
+            <p className={styles.totalPrice}>${totalPrice}</p>
+          </div>
+        </div>
+      </div>
+      <button className={styles.removeButton} onClick={handleRemoveItem}>
+        <img src={removeFromCartSVG} alt="Remove from cart" />
+      </button>
     </div>
   );
 };
