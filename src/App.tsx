95dbd45cc770f4c87c480473e9b282750598b824
nikogalvan <ramiro_coronel333@hotmail.com>
2025-07-02 18:51:04 -0300

    feat(main): add react router provider and Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { Route, Routes } from "react-router";
 import { CartProvider } from "./context/CartContext/CartContext";
 import Home from "./pages/home/Home";
 import "./styles/style.scss";
+import Example from "./pages/example/Example";
 function App() {
   return (
     <>
       <CartProvider>
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/example" element={<Example />} />
+        </Routes>
       </CartProvider>
     </>
   );
