42b125bb3ff3140135315efa918962f9f39c553c
nikogalvan <ramiro_coronel333@hotmail.com>
2025-07-02 18:50:14 -0300

    chore: remove base property troubleshoting with react-router

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/fm-product-list-with-cart/",
+  //base: "/fm-product-list-with-cart/",
   plugins: [react()],
 })
